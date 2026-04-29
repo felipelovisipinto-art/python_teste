a8c765eb6f08fd450d661eefb2fe52e02a660c8f
felipelovisipinto-art <felipelovisipinto@gmail.com>
2026-04-29 13:14:56 +0000

    Apr 29, 2026, 10:14 AM

--- aula48.py
+++ aula48.py
@@ -15,4 +15,5 @@ string = "ABCDE"  # 5 caracteres (len)
 #        0    1      2               3    4
 #       -5   -4     -3              -2   -1
 lista = [123, True, "Felipe Lovisi", 1.2, []]
+lista[-3] = "Maria"
 print(lista[2], type(lista[2]))
