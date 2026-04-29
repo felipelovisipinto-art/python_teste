2cdc83155dc4c31960241fb663b2d77cd3832871
felipelovisipinto-art <felipelovisipinto@gmail.com>
2026-04-29 13:07:19 +0000

    Apr 29, 2026, 10:07 AM

---
+++ aula48.py
@@ -0,0 +1,13 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+"""
+
+#        +01234
+#        -54321
+string = "ABCDE"  # 5 caracteres (len)
+lista = []
+print(lista, type(lista))
