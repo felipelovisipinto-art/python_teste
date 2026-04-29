5d39b15a82106a9ee20a1ac7d6d153976d64fd37
felipelovisipinto-art <felipelovisipinto@gmail.com>
2026-04-29 13:19:33 +0000

    Apr 29, 2026, 10:19 AM

--- aula48.py
+++ aula48.py
@@ -16,4 +16,5 @@ string = "ABCDE"  # 5 caracteres (len)
 #       -5   -4     -3              -2   -1
 lista = [123, True, "Felipe Lovisi", 1.2, []]
 lista[-3] = "Maria"
+print(lista)
 print(lista[2], type(lista[2]))
---
+++ aula49.py
@@ -0,0 +1,16 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis:
+    append, insert, pop, del, clear, extend, +
+Create Read Update   Delete
+Criar, ler, alterar, apagar = lista[i] (CRUD)
+"""
+
+lista = [10, 20, 30, 40]
+lista[2] = 300
+del lista[2]
+print(lista)
+print(lista[2])
