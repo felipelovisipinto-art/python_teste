52a7c55f1a157bede19fed53febbd1825dc5b5c9
felipelovisipinto-art <felipelovisipinto@gmail.com>
2026-04-29 13:21:14 +0000

    Apr 29, 2026, 10:21 AM

--- aula49.py
+++ aula49.py
@@ -9,7 +9,8 @@ Create Read Update   Delete
 Criar, ler, alterar, apagar = lista[i] (CRUD)
 """
 
-lista = [10, 20, 30, 40]
+#        0   1   2   3    4  5
+lista = [10, 20, 30, 40, 50, 60]
 lista[2] = 300
 del lista[2]
 print(lista)
