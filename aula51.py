33dc00d91fa9b48c63df9b22483951801ef44601
felipelovisipinto-art <felipelovisipinto@gmail.com>
2026-04-30 12:17:10 +0000

    Apr 30, 2026, 9:17 AM

--- aula51.py
+++ aula51.py
@@ -18,5 +18,5 @@ Criar, ler, alterar, apagar = lista[i] (CRUD)
 lista_a = [1, 2, 3]
 lista_b = [4, 5, 6]
 lista_c = lista_a + lista_b
-lista_d = lista_a.extend(lista_b)
-print(lista_d)
+lista_a.extend(lista_b)
+print(lista_a)
