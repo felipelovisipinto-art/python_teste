2467d5001327530595a4a4bec4f1da9e1218d67b
felipelovisipinto-art <felipelovisipinto@gmail.com>
2026-04-30 12:25:26 +0000

    Apr 30, 2026, 9:25 AM

--- aula52.py
+++ aula52.py
@@ -4,9 +4,9 @@ Cuidados com dados mutáveis
 = - aponta para o mesmo valor da memória (mutável)
 """
 
-lista_a = ["Felipe", "Maria"]
-lista_b = lista_a
-
+lista_a = ["Felipe", "Maria", 1, True, 1.2]
+lista_b = lista_a.copy()
 
 lista_a[0] = "qualquer coisa"
 print(lista_b)
+print(lista_a)
