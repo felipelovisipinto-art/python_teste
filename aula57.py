ed7905c392d3e3f235b698577da4361d20a6105c
felipelovisipinto-art <felipelovisipinto@gmail.com>
2026-05-05 15:33:46 +0000

    May 5, 2026, 12:33 PM

--- aula57.py
+++ aula57.py
@@ -5,12 +5,15 @@ enumerate - enumera iteráveis (índices)
 lista = ["Felipe", "Minduim", "Iara"]
 lista.append("joão")
 
-# lista_enumerada = list(enumerate(lista))
-# print(lista_enumerada)
 
-# for indice, nome in enumerate(lista):
+for indice, nome in enumerate(lista):
+    print(indice, nome)
+
+# for item in enumerate(lista):
+#     indice, nome = item
 #     print(indice, nome)
 
-for item in enumerate(lista):
-    indice, nome = item
-    print(indice, nome)
+# for tupla_enumerada in enumerate(lista):
+#     print("For da tupla: ")
+#     for valor in tupla_enumerada:
+#         print(f"\t{valor}")
