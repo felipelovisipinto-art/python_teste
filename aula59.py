60fa91a98e75a3df6ee9cd0b14cf264d36c8feae
felipelovisipinto-art <felipelovisipinto@gmail.com>
2026-05-07 15:07:06 +0000

    May 7, 2026, 12:07 PM

--- aula59.py
+++ aula59.py
@@ -5,8 +5,10 @@ https://en.wikipedia.org/wiki/Double-precision_floating-point_format
 https://docs.python.org/pt-br/3/tutorial/floatingpoint.html
 """
 
-numero_1 = 0.1
-numero_2 = 0.7
+import decimal
+
+numero_1 = decimal.Decimal(0.1)
+numero_2 = decimal.Decimal(0.7)
 numero_3 = numero_1 + numero_2
 print(numero_3)
 print(f"{numero_3:.2f}")
