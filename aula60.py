22738373cc56909228f89977eeba9454f03f5805
felipelovisipinto-art <felipelovisipinto@gmail.com>
2026-05-08 17:00:27 +0000

    May 8, 2026, 2:00 PM

--- aula60.py
+++ aula60.py
@@ -6,4 +6,8 @@ join - une uma string
 
 frase = "Olha só que, coisa interessante"
 lista_frases = frase.split(",")
+
+for i, frase in enumerate(lista_frases):
+    print(lista_frases)
+
 print(lista_frases)
