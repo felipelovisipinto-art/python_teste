59c2569a9eac6d349409d01f397904b54e1728c9
felipelovisipinto-art <felipelovisipinto@gmail.com>
2026-05-08 17:07:51 +0000

    May 8, 2026, 2:07 PM

--- aula60.py
+++ aula60.py
@@ -4,10 +4,10 @@ split - divide uma string (list)
 join - une uma string
 """
 
-frase = "Olha só que, coisa interessante"
+frase = "           Olha só que,           coisa interessante          "
 lista_frases = frase.split(",")
 
 for i, frase in enumerate(lista_frases):
-    print(lista_frases)
+    lista_frases[i] = lista_frases[i].strip()
 
 print(lista_frases)
