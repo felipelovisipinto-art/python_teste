e08313953e496f2845c5e613be92e0f69d6d2bb1
felipelovisipinto-art <felipelovisipinto@gmail.com>
2026-05-08 17:09:57 +0000

    May 8, 2026, 2:09 PM

--- aula60.py
+++ aula60.py
@@ -5,9 +5,10 @@ join - une uma string
 """
 
 frase = "           Olha só que,           coisa interessante          "
-lista_frases = frase.split(",")
+lista_frases_cruas = frase.split(",")
 
-for i, frase in enumerate(lista_frases):
-    lista_frases[i] = lista_frases[i].strip()
+lista_frases = []
+for i, frase in enumerate(lista_frases_cruas):
+    lista_frases.append(lista_frases_cruas[i].strip())
 
 print(lista_frases)
