7e0f25fe94b4382e87cf29569fbd658492c5de22
felipelovisipinto-art <felipelovisipinto@gmail.com>
2026-05-08 17:25:16 +0000

    May 8, 2026, 2:25 PM

--- aula61.py
+++ aula61.py
@@ -12,10 +12,11 @@ salas = [
     [
         "Elaine",  # 0
     ],
-    # 2
-    ["Luiz", "João", "Eduarda", (0, 2, 3, 4)],  # 0  # 1  # 2
+    # 0       1       2          0  1   2   3   4
+    ["Luiz", "João", "Eduarda", (0, 10, 20, 30, 40)],  # 2
 ]
 
 print(salas[1][0])
 print(salas[0][1])
 print(salas[2][2])
+print(salas[2][3][2])
