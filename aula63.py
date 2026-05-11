c34052c3f9a2dae2ad681e36b8677c4c3116229c
felipelovisipinto-art <felipelovisipinto@gmail.com>
2026-05-11 12:53:14 +0000

    May 11, 2026, 9:53 AM

--- aula63.py
+++ aula63.py
@@ -1,8 +1,32 @@
 # Desempacotamentos em chamadas
 # de métodos e funções
 string = "ABCD"
-lista = ["Maria", "Helena", "Eduarda"]
+lista = ["Maria", "Helena", 1, 2, 3, "Eduarda"]
 tupla = "python", "é", "legal"
+salas = [
+    # 0
+    [
+        "Maria",  # 0
+        "Helena",  # 1
+    ],
+    # 1
+    [
+        "Elaine",  # 0
+    ],
+    # 2
+    [
+        "Luiz",  # 0
+        "João",  # 1
+        "Eduarda",  # 2
+    ],
+]
 
-a, b, c = lista
-print(a, c)
+# p, b, *_, ap, u = lista
+# print(p, u, ap)
+
+# print("Maria", "Helena", 1, 2, 3, "Eduarda")
+# print(*lista)
+# print(*string)
+# print(*tupla)
+
+print(*salas, sep="\n")
