e75429f0e059033473115c99a031a561d2c9d4b3
felipelovisipinto-art <felipelovisipinto@gmail.com>
2026-05-27 13:10:13 -0300

    .

---
+++ aula73.py
@@ -0,0 +1,16 @@
+"""
+Higher order functions
+Funções de primeira classe
+"""
+
+
+def saudacao(msg, nome):
+    return f"{msg}, {nome}!"
+
+
+def executa(funcao, *args):
+    return funcao(*args)
+
+
+print(executa(saudacao, "Bom dia", "Felipe"))
+print(executa(saudacao, "Boa noite", "Minduim"))
